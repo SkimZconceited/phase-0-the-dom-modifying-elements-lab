86a3be3f73b7681ea7f4606ea1cc2374b8aeda16
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-02 12:28:37 +0300

    Complets assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const rmMain = document.querySelector('main');
+rmMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = `Stanley 'SkimZ' Kimambo is the champion.`;
+
+console.log(newHeader.nodeName);
+console.log(newHeader.id);
+console.log(newHeader.innerHTML);
